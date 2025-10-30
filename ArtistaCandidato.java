588197c833e8643930040a3510a6e9d508339b69
Luana Dominguez <dominguezam.01@gmail.com>
2025-10-29 23:59:44 -0300

    Update ArtistaCandidato.java

--- ArtistaCandidato.java
+++ ArtistaCandidato.java
@@ -8,8 +8,24 @@ public class ArtistaCandidato extends ArtistaBase{
 	private int cantMaxCanciones;
 	private int cantCancionesAsignadas;
 	
-	public double calcularCostoContratacion() {
-		return 0.0;
+	public ArtistaCandidato(String nombre, ArrayList <Rol> rolesHistoricos, ArrayList <Banda> bandasHistoricas,
+			double costoBasePorCancion, int cantMaxCanciones) {
+		super(nombre, rolesHistoricos, bandasHistoricas);
+		this.costoBasePorCancion = costoBasePorCancion;
+		this.cantMaxCanciones = cantMaxCanciones;
+	}
+	
+	public double calcularCostoContratacion(ArrayList<ArtistaBase> artistasBase) {
+		double costoIni = costoBasePorCancion;
+		//si ya toco con un ArtistaBase en una banda, su costo se reduce al 50%
+		for(ArtistaBase base : artistasBase) {
+			for(Banda b : this.getBandasHistoricas()) {
+				if(base.getBandasHistoricas().contains(b)) {
+					return costoIni / 2;
+				}
+			}
+		}
+		return costoIni;
 	}
 	public boolean compartioBandaConArtistaBase() {
 		return false;
