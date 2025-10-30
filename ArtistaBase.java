fc13ee441f1c303d4bdf991118f2592f450a1016
Luana Dominguez <dominguezam.01@gmail.com>
2025-10-29 23:59:19 -0300

    Update ArtistaBase.java

--- ArtistaBase.java
+++ ArtistaBase.java
@@ -7,6 +7,15 @@ public class ArtistaBase {
 	private ArrayList <Rol> rolesHistoricos = new ArrayList<Rol>();
 	private ArrayList <Banda> bandasHistoricas = new ArrayList <Banda>();
 	
+	public ArtistaBase(String nombre, ArrayList <Rol> rolesHistoricos, ArrayList <Banda> bandasHistoricas) {
+		if(rolesHistoricos == null || bandasHistoricas == null) {
+	        throw new IllegalArgumentException("Las listas de roles y bandas no pueden ser null");
+	    }
+		this.nombre = nombre;
+		this.bandasHistoricas = bandasHistoricas;
+		this.rolesHistoricos = rolesHistoricos;
+	}
+	
 	public void agregarRol(Rol nuevoRol) {
 		rolesHistoricos.add(nuevoRol);
 	}
@@ -20,5 +29,7 @@ public class ArtistaBase {
 	public ArrayList<Banda> getBandasHistoricas() {
 		return bandasHistoricas;
 	}
-	
+	public void agregarBanda(Banda bandaNueva) {
+		bandasHistoricas.add(bandaNueva);
+	}
 }
